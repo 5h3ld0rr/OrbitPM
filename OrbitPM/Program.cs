30cfce798bdb37d34d5435e874f428d255c333e6
5h3ld0r <chirathtimodh@gmail.com>
2026-04-14 02:24:22 +0530

    fix: db migration

--- OrbitPM/Program.cs
+++ OrbitPM/Program.cs
@@ -44,4 +44,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+}
+
 app.Run();
